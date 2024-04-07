8a5d5fc82fb471b7c8312070dec0ce4eaca9938c
Maksim <maksim@MacBook-Pro-Maksim.local>
2024-04-07 16:23:31 +0300

    some small refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,17 @@
-import { ChangeEvent, useEffect, useState } from "react"
-import { OptionType } from "./types"
+import Forecast from "./components/Forecast.tsx"
 import Search from "./components/Search.tsx"
-import 'dotenv'
+import useForecast from "./hooks/useForecast.ts"
 
-function App(): JSX.Element {
-  const APIKey = import.meta.env.VITE_PRIVATE_KEY
-  const [term, setTerm] = useState<string>("")
-  const [options, setOptions] = useState<[]>([])
-  const [city, setCity] = useState<OptionType | null>(null)
-  const [forecast, setForecast] = useState<null>(null)
-
-  const getSearchOptions = (value: string) => {
-    fetch(
-      `https://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${APIKey}`
-    )
-      .then((res) => res.json())
-      .then((data) => setOptions(data))
-  }
-
-  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value.trim()
-    setTerm(value)
-    if (value == "") return
-
-    getSearchOptions(value)
-  }
-
-  const getForecast = (city: OptionType) => {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&appid=${APIKey}`
-    )
-      .then((res) => res.json())
-      .then((data) => setForecast(data))
-  }
-
-  const onSubmit = () => {
-    if (!city) return
-
-    getForecast(city)
-  }
-
-  const onOptionSelect = (option: OptionType) => {
-    setCity(option)
-  }
-
-  useEffect(() => {
-    if (city) {
-      setTerm(city.name)
-      setOptions([])
-    }
-  }, [city])
+export default function App(): JSX.Element {
+  const {
+    term, options, forecast, onInputChange, onOptionSelect, onSubmit
+  } = useForecast()
 
   return (
     <>
-      <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
+      <main className={`flex overflow-y-auto justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100dvh] w-full ${forecast && "pt-12" }`}>
         {forecast ? (
-          "We have forecast"
+          <Forecast data={forecast} />
         ) : (
           <Search
             term={term}
@@ -69,5 +25,3 @@ function App(): JSX.Element {
     </>
   )
 }
-
-export default App
---
+++ src/hooks/useForecast.ts
@@ -0,0 +1,67 @@
+import "dotenv"
+import { useState, useEffect, ChangeEvent } from "react"
+import { OptionType, ForecastType } from "../types"
+
+export default function useForecast() {
+  const APIKey = import.meta.env.VITE_PRIVATE_KEY
+  const [term, setTerm] = useState<string>("")
+  const [options, setOptions] = useState<[]>([])
+  const [city, setCity] = useState<OptionType | null>(null)
+  const [forecast, setForecast] = useState<ForecastType | null>(null)
+
+  const getSearchOptions = (value: string) => {
+    fetch(
+      `https://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${APIKey}`
+    )
+      .then((res) => res.json())
+      .then((data) => setOptions(data))
+  }
+
+  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.trim()
+    setTerm(value)
+    if (value == "") return
+
+    getSearchOptions(value)
+  }
+
+  const getForecast = (city: OptionType) => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${city.lat}&lon=${city.lon}&units=metric&appid=${APIKey}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        const forecastData = {
+          ...data.city,
+          list: data.list.slice(0, 16),
+        }
+        setForecast(forecastData)
+      })
+  }
+
+  const onSubmit = () => {
+    if (!city) return
+
+    getForecast(city)
+  }
+
+  const onOptionSelect = (option: OptionType) => {
+    setCity(option)
+  }
+
+  useEffect(() => {
+    if (city) {
+      setTerm(city.name)
+      setOptions([])
+    }
+  }, [city])
+
+  return {
+    term,
+    options,
+    forecast,
+    onInputChange,
+    onOptionSelect,
+    onSubmit,
+  }
+}
